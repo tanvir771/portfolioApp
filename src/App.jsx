9f3c0c5a79c710b8940317814fd3b6e7776c0fc7
tanvir771 <tanvir.leon53@gmail.com>
2025-01-16 03:27:53 -0800

    added sticky icons and fixed position for github link; and hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Gallery from '../components/Gallery';
 import Photos from './Photos';
@@ -57,6 +57,17 @@ function App() {
     const hiddenElements = document.querySelectorAll('.myHidden');
     hiddenElements.forEach((el) => observer.observe(el));
 
+    const scroll = document.querySelectorAll(".scroll-down");
+    scroll.forEach(val => {
+      val.addEventListener('click', function() {
+        let pageHeight = window.innerHeight;
+        window.scrollBy({
+          top: pageHeight,
+          behavior: 'smooth'
+        });
+      })
+    });
+
     return () => {
       // Cleanup observer when component unmounts
       observer.disconnect();
@@ -69,7 +80,7 @@ function App() {
         <section className="myHidden first">
           <h1 className='sm:text-2xl text-xl mb-4'>Hi, I'm Tanvir Leon!</h1>
           <p className='text-xs sm:text-sm md:text-base'>I'm a Computer Science major @SFU, with a minor in Mathematics.</p>
-          <div className="info">
+          <div className="sticky top-2 info">
             <div className="myHidden">
               <a href="https://github.com/tanvir771"><i class="smlogo devicon-github-original"></i></a>
             </div>
@@ -77,14 +88,14 @@ function App() {
               <a href="https://www.linkedin.com/in/tanvir-leon"><i class="smlogo devicon-linkedin-plain"></i></a>
             </div>
           </div>
-          <div class="scroll-down"> <p>Scroll down</p> <div class="arrow"></div> </div>
+          <div class="scroll-down mt-8 cursor-pointer"><p>Scroll down</p> <div class="arrow"></div> </div>
         </section>
       </main>
 
       <section className="myHidden">
         <p className="text-xs sm:text-sm md:text-base">You can take a look at some of my projects here</p>
         <p className="text-xs sm:text-sm md:text-base mb-4">I mainly like working with: </p>
-        <div className="logos">
+        <div className="sticky top-2 logos">
           <div className="logo myHidden">
             <i className="lglogo devicon-cplusplus-plain"></i>
           </div>
@@ -98,22 +109,23 @@ function App() {
             <i className="lglogo devicon-csharp-plain"></i>
           </div>
         </div>
-        <div class="scroll-down mt-8"> <p>Scroll down</p> <div class="arrow"></div> </div>
+        <div class="scroll-down mt-8 cursor-pointer"><p>Scroll down</p> <div class="arrow"></div> </div>
       </section>
 
       <section className="myHidden">
         <h1 className='sm:text-2xl text-xl mb-4'>Showcase of my projects</h1>
-        <div className='infogit'>
+        <div className='infogit flex items-center justify-between'>
           <p className='text-xs sm:text-sm md:text-base'>Find more information on my Github</p>
           <a href="https://github.com/tanvir771"><i class="smlogo devicon-github-original"></i></a>
         </div>
         <ProjectCollection projects={projects} />
+        <div class="scroll-down mt-8 cursor-pointer"><p>Scroll down</p> <div class="arrow"></div> </div>
       </section>
 
       <section className="myHidden">
         <p className='text-center text-xs sm:text-sm md:text-base'>Beyond coding, I love capturing cityscapes through photography</p>
         <p className='text-center text-xs sm:text-sm md:text-base'>check them out!</p>
-        <div class="scroll-down mt-8"> <p>Scroll down</p> <div class="arrow"></div> </div>
+        <div class="scroll-down mt-8 cursor-pointer"><p>Scroll down</p> <div class="arrow"></div> </div>
       </section>
 
       <section className="myHidden"><Photos /></section>
